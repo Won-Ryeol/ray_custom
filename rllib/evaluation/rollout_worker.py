dd2a7cc80c3b29bbff6d283f6d1938e78f198ca0
mch5048 <mch5048@korea.ac.kr>
2021-11-20 21:44:51 +0900

    add for debugging

--- rllib/evaluation/rollout_worker.py
+++ rllib/evaluation/rollout_worker.py
@@ -992,7 +992,8 @@ class RolloutWorker(ParallelIteratorWorker):
             if ARCH.FILTER_GATSBI_AGENT:
                 state = OrderedDict(filter(lambda p: (
                     # (p[0].split('.')[0] == 'obj_module' and p[0].split('.')[1] != 'extract_global_agent_feature' and
-                    # p[0].split('.')[1] != 'latent_post_prop' and p[0].split('.')[1] != 'latent_post_prop_action') or 
+                   ( p[0].split('.')[2] != 'uncertain_attention' if len(p[0].split('.')) >=3 
+                    else p[0].split('.')[0] == 'obj_module')  and 
                     p[0].split('.')[0] == 'obj_module' or
                     p[0].split('.')[0] == 'mixture_module' or 
                     p[0].split('.')[0] == 'keypoint_module' or
